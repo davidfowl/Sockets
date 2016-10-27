17c0756a728cf34b560fb836524c5614a7e08e0e
BrennanConroy <brecon@microsoft.com>
2016-10-27 16:10:25 -0700

    slice and dice

--- samples/SocketsSample/EndPoints/ChatEndPoint.cs
+++ samples/SocketsSample/EndPoints/ChatEndPoint.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Channels;
 using Microsoft.AspNetCore.Sockets;
 using System.Collections.Concurrent;
+using Channels.Text.Primitives;
 
 namespace SocketsSample
 {
@@ -23,77 +24,34 @@ namespace SocketsSample
 
         private ConcurrentDictionary<string, Connection> _users = new ConcurrentDictionary<string, Connection>();
 
-        private bool ByteCompare(Span<byte> buffer, int start, int end, Span<byte> buffer2, int start2, int end2)
+        private int FindSpace(ReadableBufferReader input)
         {
-            if (start > end || start2 > end2 || end - start < end2 - start2)
+            while (input.Peek() != Space && !input.End)
             {
-                return false;
+                input.Take();
             }
 
-            while (start < end)
+            if (input.End)
             {
-                if (!buffer[start].Equals(buffer2[start2]))
-                {
-                    return false;
-                }
-
-                start2++;
-                start++;
+                return -1;
             }
 
-            return true;
+            return input.Index;
         }
 
-        private List<Span<byte>> GetTokens(byte[] message)
+        private int FindNonSpace(ref ReadableBufferReader input)
         {
-            var list = new List<Span<byte>>();
-            var start = 0;
-            var length = message.Length;
-            for (var index = start; index < length; index++)
+            while (input.Peek() == Space)
             {
-                if (message[index].Equals(Space))
-                {
-                    if (index != start)
-                    {
-                        list.Add(message.Slice(start, index - start));
-                    }
-                    start = index + 1;
-                    continue;
-                }
+                input.Take();
             }
 
-            if (length != start)
+            if (input.End)
             {
-                list.Add(message.Slice(start, length - start));
+                return -1;
             }
 
-            return list;
-        }
-
-        private int FindSpace(Span<byte> input)
-        {
-            for (var i = 0; i < input.Length; ++i)
-            {
-                if (input[i].Equals(Space))
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
-
-        private int FindNonSpace(Span<byte> input)
-        {
-            for (var i = 0; i < input.Length; ++i)
-            {
-                if (!input[i].Equals(Space))
-                {
-                    return i;
-                }
-            }
-
-            return -1;
+            return input.Index;
         }
 
         public override async Task OnConnected(Connection connection)
@@ -113,84 +71,83 @@ namespace SocketsSample
                     }
 
                     // We can avoid the copy here but we'll deal with that later
-                    var message = input.ToArray();
+                    //var message = input.ToArray();
 
-                    if (message[0].Equals(ForwardSlash))
+                    if (input.Peek() == ForwardSlash)
                     {
-                        var index = FindSpace(message.Slice(1));
-                        if (index != -1)
+                        ReadableBuffer buffer;
+                        ReadCursor cursor;
+                        if (input.TrySliceTo(Space, out buffer, out cursor))
                         {
-                            index += 1;
+                            //only allow /nick until user has a name
                             if (name == null)
                             {
-                                // check if /nick
-                                if (message.Slice(0, index).BlockEquals(Nick.Slice()))
+                                if (buffer.Equals(Nick))
                                 {
-                                    var nameIndex = FindNonSpace(message.Slice(index));
-                                    if (nameIndex != -1)
+                                    buffer = input.Slice(cursor);
+                                    buffer = buffer.TrimStart();
+
+                                    if (buffer.Length > 0)
                                     {
-                                        index += nameIndex;
-                                        var index3 = FindSpace(message.Slice(index));
-                                        if (index3 == -1)
+                                        var i = FindSpace(new ReadableBufferReader(buffer));
+                                        if (i == -1)
                                         {
-                                            name = message.Slice(index).ToArray();
+                                            name = buffer.ToArray();
                                         }
                                         else
                                         {
-                                            name = message.Slice(index, index3).ToArray();
+                                            name = buffer.Slice(0, i).ToArray();
                                         }
-                                        _users.AddOrUpdate(Encoding.UTF8.GetString(name), connection, (k, c) => { throw new Exception("name already added"); });
-                                        await Broadcast($"Set username to {Encoding.UTF8.GetString(name)}", connection);
+
+                                        var stringName = Encoding.UTF8.GetString(name);
+                                        _users.AddOrUpdate(stringName, connection, (k, c) => { throw new Exception("name already added"); });
+                                        await Broadcast($"Set username to {stringName}", connection);
                                     }
                                 }
                             }
-                            else //already have a name
+                            else
                             {
-                                // check if /msg
-                                if (message.Slice(0, index).BlockEquals(Msg.Slice()))
+                                if (buffer.Equals(Msg))
                                 {
-                                    var nameIndex = FindNonSpace(message.Slice(index));
-                                    if (nameIndex != -1)
+                                    buffer = input.Slice(cursor);
+                                    buffer = buffer.TrimStart();
+
+                                    var i = FindSpace(new ReadableBufferReader(buffer));
+                                    if (i == -1)
                                     {
-                                        index += nameIndex;
-                                        byte[] pmName = null;
-                                        var index3 = FindSpace(message.Slice(index));
-                                        if (index3 == -1)
+                                        // no user specified
+                                    }
+                                    else
+                                    {
+                                        var pmName = buffer.Slice(0, i).ToArray();
+
+                                        Connection c;
+                                        if (!_users.TryGetValue(Encoding.UTF8.GetString(pmName), out c))
                                         {
-                                            pmName = message.Slice(index).ToArray();
+                                            //failed to get user
                                         }
-                                        else
+
+                                        buffer = buffer.Slice(i).TrimStart();
+
+                                        if (buffer.Length > 0)
                                         {
-                                            pmName = message.Slice(index, index3).ToArray();
-                                        }
-                                        index += index3;
-                                        var index4 = FindNonSpace(message.Slice(index));
-                                        if (index4 == -1)
-                                        {
-                                            //no message
-                                        }
-                                        else
-                                        {
-                                            index += index4;
-                                            Connection c;
-                                            if (!_users.TryGetValue(Encoding.UTF8.GetString(pmName), out c))
-                                            {
-                                                //failed to get user
-                                            }
-                                            var msg = message.Slice(index);
-                                            await Broadcast(new List<Span<byte>> { PMFromSeparator, name, NameSeparator, msg }, c);
-                                            await Broadcast(new List<Span<byte>> { PMToSeparator, pmName, NameSeparator, msg }, connection);
+                                            var message = buffer.ToArray();
+
+                                            await Broadcast(PMFromSeparator, name, NameSeparator, message, c);
+                                            await Broadcast(PMToSeparator, pmName, NameSeparator, message, connection);
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                    else // no '/'
+                    // no '/'
+                    else
                     {
                         if (name != null)
                         {
-                            await Broadcast(new List<Span<byte>> { name, NameSeparator, message });
+                            // WriteableBuffer can take a ReadableBuffer in Append look into fixing this
+                            await Broadcast(name, NameSeparator, input.ToArray());
                         }
                     }
                 }
@@ -200,14 +157,17 @@ namespace SocketsSample
                 }
             }
 
-            Connection temp;
-            var nameStr = Encoding.UTF8.GetString(name);
-            if (!_users.TryRemove(nameStr, out temp))
+            if (name != null)
             {
-                // (TODO) failed to remove from dict
+                Connection temp;
+                var nameStr = Encoding.UTF8.GetString(name);
+                if (!_users.TryRemove(nameStr, out temp))
+                {
+                    // (TODO) failed to remove from dict
+                }
             }
 
-            await Broadcast($"{nameStr}: {connection.ConnectionId} disconnected ({connection.Metadata["transport"]})");
+            await Broadcast($"{connection.ConnectionId} disconnected ({connection.Metadata["transport"]})");
         }
 
         private Task Broadcast(string text)
@@ -227,13 +187,25 @@ namespace SocketsSample
             return Task.WhenAll(tasks);
         }
 
-        private Task Broadcast(IEnumerable<Span<byte>> payload)
+        private Task Broadcast(Span<byte> payload1, Span<byte> payload2)
         {
             var tasks = new List<Task>(Connections.Count);
 
             foreach (var c in Connections)
             {
-                tasks.Add(Broadcast(payload, c));
+                tasks.Add(Broadcast(payload1, payload2, c));
+            }
+
+            return Task.WhenAll(tasks);
+        }
+
+        private Task Broadcast(Span<byte> payload1, Span<byte> payload2, Span<byte> payload3)
+        {
+            var tasks = new List<Task>(Connections.Count);
+
+            foreach (var c in Connections)
+            {
+                tasks.Add(Broadcast(payload1, payload2, payload3, c));
             }
 
             return Task.WhenAll(tasks);
@@ -249,13 +221,30 @@ namespace SocketsSample
             return c.Channel.Output.WriteAsync(payload);
         }
 
-        private Task Broadcast(IEnumerable<Span<byte>> payload, Connection c)
+        private Task Broadcast(Span<byte> payload1, Span<byte> payload2, Connection c)
         {
             var writeBuffer = c.Channel.Output.Alloc();
-            foreach (var val in payload)
-            {
-                writeBuffer.Write(val);
-            }
+            writeBuffer.Write(payload1);
+            writeBuffer.Write(payload2);
+            return writeBuffer.FlushAsync();
+        }
+
+        private Task Broadcast(Span<byte> payload1, Span<byte> payload2, Span<byte> payload3, Connection c)
+        {
+            var writeBuffer = c.Channel.Output.Alloc();
+            writeBuffer.Write(payload1);
+            writeBuffer.Write(payload2);
+            writeBuffer.Write(payload3);
+            return writeBuffer.FlushAsync();
+        }
+
+        private Task Broadcast(Span<byte> payload1, Span<byte> payload2, Span<byte> payload3, Span<byte> payload4, Connection c)
+        {
+            var writeBuffer = c.Channel.Output.Alloc();
+            writeBuffer.Write(payload1);
+            writeBuffer.Write(payload2);
+            writeBuffer.Write(payload3);
+            writeBuffer.Write(payload4);
             return writeBuffer.FlushAsync();
         }
     }
